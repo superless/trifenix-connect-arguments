00a1ec9e80de0a940b7d592b1bf5ebbf0fe438ef
alejandro.iglesias <alejandro.iglesias@trifenix.io>
2020-12-17 17:31:35 -0300

    inicializando repo

---
+++ CosmosDbArguments.cs
@@ -0,0 +1,34 @@
+﻿namespace trifenix.connect.arguments
+{
+
+    /// <summary>
+    /// Argumentos de cosmosDb
+    /// </summary>
+    public class CosmosDbArguments {
+        /// <summary>
+        /// EndPoint of the db
+        /// </summary>
+        /// <value>
+        /// The end point URL.
+        /// </value>
+        public string EndPointUrl { get; set; }
+
+        /// <summary>
+        /// the cosmos db primary key
+        /// </summary>
+        /// <value>
+        /// The primary key.
+        /// </value>
+        public string PrimaryKey { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the name of the cosmos database.
+        /// </summary>
+        /// <value>
+        /// The name database.
+        /// </value>
+        public string NameDb { get; set; }
+                      
+    }
+}
---
+++ Namespace.cs
@@ -0,0 +1,9 @@
+namespace trifenix.connect.arguments
+{
+    /// <summary>
+    /// El namespace de argumentos, permite dejar en un solo repositorio los 
+    /// argumentos de inicialización de un servicio cloud u otro.
+    /// </summary>
+    internal class Namespace {                     
+    }
+}
